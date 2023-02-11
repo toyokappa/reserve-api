f7f6499d8c3846e0d94945312711a57fc5765b28
Yuta Toyokawa <kppg42@gmail.com>
2023-02-12 01:32:51 +0900

    シードの追加

--- db/fixtures/01_staff.rb
+++ db/fixtures/01_staff.rb
@@ -7,3 +7,13 @@ Staff.seed_once do |s|
   s.password = 'password'
   s.confirmed_at = Time.zone.now
 end
+
+Staff.seed_once do |s|
+  s.id = 2
+  s.first_name = '梅子'
+  s.last_name = '松田'
+  s.display_name = 'Umeko'
+  s.email = 'umeko@example.com'
+  s.password = 'password'
+  s.confirmed_at = Time.zone.now
+end
