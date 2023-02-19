6eaa54bbc9cec03482c596511de5eb659c6b6f53
Yuta Toyokawa <kppg42@gmail.com>
2023-02-19 21:03:08 +0900

    チケット枚数が不足しても購入できてしまうバグの修正

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,9 +5,28 @@ class Reservation < ApplicationRecord
   belongs_to :customer, optional: true
   belongs_to :guest, optional: true
 
+  validate :required_either_customer_or_guest
+  validate :required_number_of_necessary_tickets
+
   def scheduled_time
     from = scheduled_date.to_fs(:time)
     to = (scheduled_date + required_time.minutes).to_fs(:time)
     "#{from}~#{to}"
+  end
+
+  private
+
+  def required_either_customer_or_guest
+    return if customer.present? ^ guest.present?
+
+    errors.add(:base, '会員もしくはゲストのどちらか一方を入力してください')
+  end
+
+  def required_number_of_necessary_tickets
+    required_ticket = program.required_ticket
+    used_ticket = tickets.length
+    return if required_ticket < used_ticket
+    
+    errors.add(:base, 'お手持ちのチケットの枚数が不足しています')
   end
 end
