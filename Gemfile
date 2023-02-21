d98a290f27232cca2471c9bff1c202a1096a09f6
Yuta Toyokawa <kppg42@gmail.com>
2023-02-21 23:06:43 +0900

    画像のリサイズ

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem "bootsnap", require: false
 gem "rails-i18n"
 gem "enum_help"
 gem "carrierwave"
+gem "mini_magick"
 gem "slim-rails"
 
 gem "rack-cors"
---
+++ app/uploaders/application_uploader.rb
@@ -0,0 +1,34 @@
+class ApplicationUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_allowlist
+    %w(jpg jpeg gif png)
+  end
+
+  process :fix_rotate
+
+  def filename
+    "#{secure_token}.jpg" if original_filename.present?
+  end
+
+  def fix_rotate
+    manipulate! do |img|
+      img = img.auto_orient
+      img = yield(img) if block_given?
+      img
+    end
+  end
+
+  protected
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
+end
