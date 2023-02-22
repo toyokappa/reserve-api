32f6dc40ace64a4680508934d0da3a145ab66bbb
Yuta Toyokawa <kppg42@gmail.com>
2023-02-22 23:09:41 +0900

    本番用に色々と修正

--- Gemfile
+++ Gemfile
@@ -17,13 +17,13 @@ gem "slim-rails"
 
 gem "rack-cors"
 
+gem 'seed-fu'
 gem "devise_token_auth"
 gem "devise-i18n"
 gem "devise-i18n-views"
 gem "payjp"
 
 group :development, :test do
-  gem 'seed-fu'
   gem "pry-rails"
   gem "pry-byebug"
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- bin/start_server.sh
+++ bin/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ln -sf /dev/stdout /usr/src/app/log/${RAILS_ENV}.log
 RAILS_ENV=${RAILS_ENV} bundle exec rails db:create
-RAILS_ENV=${RAILS_ENV} bundle exec rails db:migrate:reset # TODO: 忘れずに戻す
+RAILS_ENV=${RAILS_ENV} DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset # TODO: 忘れずに戻す
 RAILS_ENV=${RAILS_ENV} bundle exec rails db:seed_fu
 RAILS_ENV=${RAILS_ENV} bundle exec rails s
