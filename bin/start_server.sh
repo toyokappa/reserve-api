423f899fe21b5e826c0b19de6abba5f191bf567e
Yuta Toyokawa <kppg42@gmail.com>
2023-02-22 22:53:11 +0900

    データリセット

--- bin/start_server.sh
+++ bin/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ln -sf /dev/stdout /usr/src/app/log/${RAILS_ENV}.log
 RAILS_ENV=${RAILS_ENV} bundle exec rails db:create
-RAILS_ENV=${RAILS_ENV} bundle exec rails db:migrate
+RAILS_ENV=${RAILS_ENV} bundle exec rails db:migrate:reset # TODO: 忘れずに戻す
 RAILS_ENV=${RAILS_ENV} bundle exec rails db:seed_fu
 RAILS_ENV=${RAILS_ENV} bundle exec rails s
